01ec9cce9a7c129f57f6593a208b165ad6c9095e
Welington Marquezini Valverde da Silva <welington_marquezini88@live.com>
2022-02-01 19:52:02 -0300

    Ajuste erro

---
+++ AspNetMvcRoles/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace AspNetMvcRoles.Models
+{
+    [NotMapped]
+    public class ApplicationUser : IdentityUser
+    {
+        [Required(ErrorMessage = " O Campo {0} é Obrigatório!")]
+        public override string Email { get => base.Email; set => base.Email = value; }
+    }
+}
